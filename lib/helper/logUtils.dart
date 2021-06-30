dca24e5e94dd2afb06f76e4d50bad6999a690b08
xuehao <xuehao@soyoung.com>
2021-06-30 16:05:50 +0800

    添加日志

--- lib/helper/logUtils.dart
+++ lib/helper/logUtils.dart
@@ -1,27 +1,45 @@
 import 'package:logger/logger.dart';
 
 class LogUtils {
+  static Logger _logger = Logger(
+    filter: null, // Use the default LogFilter (-> only log in debug mode)
+    output: null, // Use the default LogOutput (-> send everything to console)
+    printer: PrettyPrinter(
+        methodCount: 2,
+        // number of method calls to be displayed
+        errorMethodCount: 8,
+        // number of method calls if stacktrace is provided
+        lineLength: 120,
+        // width of the output
+        colors: true,
+        // Colorful log messages
+        printEmojis: true,
+        // Print an emoji for each log message
+        printTime: false // Should each log print contain a timestamp
+        ),
+  );
+
   static void v(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().v(message, error, stackTrace);
+    _logger.v(message, error, stackTrace);
   }
 
   static void e(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().e(message, error, stackTrace);
+    _logger.e(message, error, stackTrace);
   }
 
   static void d(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().d(message, error, stackTrace);
+    _logger.d(message, error, stackTrace);
   }
 
   static void w(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().w(message, error, stackTrace);
+    _logger.w(message, error, stackTrace);
   }
 
   static void i(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().i(message, error, stackTrace);
+    _logger.i(message, error, stackTrace);
   }
 
   static void wtf(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    Logger().wtf(message, error, stackTrace);
+    _logger.wtf(message, error, stackTrace);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_common_lib/entity/banner_entity_entity.dart';
+import 'package:flutter_common_lib/helper/logUtils.dart';
 import 'package:logger/logger.dart';
 
 import 'net/httpRequest.dart';
@@ -13,16 +14,6 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key key}) : super(key: key) {
     HttpRequest.getInstance().putHeadr("name", "xuehao");
-    var logger = Logger(
-      printer: PrettyPrinter(
-          methodCount: 2, // number of method calls to be displayed
-          errorMethodCount: 8, // number of method calls if stacktrace is provided
-          lineLength: 120, // width of the output
-          colors: true, // Colorful log messages
-          printEmojis: true, // Print an emoji for each log message
-          printTime: false // Should each log print contain a timestamp
-      ),
-    );
   }
 
   @override
